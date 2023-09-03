e14a660bc854bfd1ad7a1e75c5ef6cdc3b139cdb
ganeshnithya185 <ganeshnithya185@gmail.com>
2023-09-03 10:24:14 +0530

    first commit

---
+++ zomatoclone/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TextControlsExample from "./component/TextControlsExample";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading1">Hello</h1>
+      <TextControlsExample />
+    </div>
+  );
+}
+
+export default App;
